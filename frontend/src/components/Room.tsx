97696a27b142ff8025d26836ff47928544e968e6
joinwithyogeshRK <joinwithyogesh17@gmail.com>
2026-03-02 14:25:56 +0530

    unsuccessfull try of adding ice candidates

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -10,13 +10,28 @@ const Room = () => {
     console.log("Room component mounted");
     const socket = io(url);
     const pc = new RTCPeerConnection();
+    pc.createDataChannel("dummy");
+   
+
     pcRef.current = pc;
     socket.on("send-offer", async ({ roomId }) => {
+        pc.onicecandidate = async (e) => {
+          console.log("setting up ice-candidates", e.candidate);
+          if (e.candidate) {
+            socket.emit("onIceCandidate", {
+              candidate: e.candidate,
+              type: "sender",
+              roomId: roomId,
+            });
+          }
+        };
       console.log("room id in frontend", roomId); //ok
 
       console.log("i recieved the send offer event"); //ok
       // const pc = new RTCPeerConnection();
-      console.log("this is peer connectiuon object", pc);
+      console.log("this is peer connectiuon object yes", pc);
+     
+      console.log("this is pc object afetr ice", pc);
 
       const sdp = await pc.createOffer();
       await pc.setLocalDescription(sdp);
@@ -40,6 +55,14 @@ const Room = () => {
         console.log("new rtc peer connection is made");
         console.log("show me the remote sdp", remoteSdp);
         await pc.setRemoteDescription(remoteSdp);
+         socket.on("add-ice-candidate", async ({ candidate, type }) => {
+           if (type === "sender") {
+             console.log("this is ur candidate", candidate);
+             pc.addIceCandidate(candidate);
+           }
+         });
+
+      
         const sdp = await pc.createAnswer();
         console.log("this is peer connectiuon object", pc);
 
@@ -49,18 +72,24 @@ const Room = () => {
           sdp,
         });
         console.log("answer is sent back tot he user 1 with the id", socket.id);
-       
       },
     );
     socket.on("answer", async ({ sdp, roomId }) => {
       console.log("sdp inside answer", sdp);
       const pc = pcRef.current!;
       await pc.setRemoteDescription(sdp);
+       socket.on("add-ice-candidate", async ({ candidate, type }) => {
+         if (type === "reciever") {
+           console.log("this is ur candidate", candidate);
+           pc.addIceCandidate(candidate);
+         }
+       });
       console.log(
         "answer is set in remote and local description for the user 2 with the socket id is ",
         socket.id,
       );
     });
+    
   }, []);
   return (
     <div>
