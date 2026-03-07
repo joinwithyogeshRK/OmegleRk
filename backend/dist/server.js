a535495a22dfcb3a7e52d4cf648449554c6fffc2
joinwithyogeshRK <joinwithyogesh17@gmail.com>
2026-03-07 18:21:51 +0530

    turn servers are added

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -16,6 +16,11 @@ const io = new Server(server, {
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+app.get("/api/turn-credentials", async (req, res) => {
+    const response = await fetch(`https://yogeshgurani.metered.live/api/v1/turn/credentials?apiKey=mV_ysrqRnRbbq4hM6dATE9e0sK7Q3rZhlBaiemMqlQLxAPvk`);
+    const iceServers = await response.json();
+    res.json(iceServers);
+});
 const userManager = new UserManager();
 io.on("connection", (socket) => {
     userManager.addUser("randomName", socket);
