7ec7497ce079cab6d6417430a03c612d0d529a40
joinwithyogeshRK <joinwithyogesh17@gmail.com>
2026-03-07 22:32:37 +0530

    skip issues resolved

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import cors from 'cors';
+import cors from "cors";
 import { UserManager } from "./managers/UserManagers.js";
 const app = express();
 app.use(cors());
@@ -10,7 +10,7 @@ app.use(express.json());
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://omegle-rk-36ut.vercel.app/",
     },
 });
 app.get("/", (req, res) => {
@@ -19,14 +19,12 @@ app.get("/", (req, res) => {
 app.get("/api/turn-credentials", async (req, res) => {
     const response = await fetch(`https://yogeshgurani.metered.live/api/v1/turn/credentials?apiKey=mV_ysrqRnRbbq4hM6dATE9e0sK7Q3rZhlBaiemMqlQLxAPvk`);
     const iceServers = await response.json();
-    console.log("ice servers", iceServers);
     res.json(iceServers);
 });
 const userManager = new UserManager();
 io.on("connection", (socket) => {
     userManager.addUser("randomName", socket);
-    socket.on("message", (sdp) => {
-    });
+    socket.on("message", (sdp) => { });
     socket.on("disconnect", () => {
         userManager.removeUser(socket.id);
     });
